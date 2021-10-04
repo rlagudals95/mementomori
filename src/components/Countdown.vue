82ca66bcab0074b28f06941897349372d6370efa
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2021-10-05 00:42:25 +0900

    counting

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -1,7 +1,17 @@
 <template>
 <div>
-  {{ countTest }}
-  {{menus}}
+  <!-- <h1>Sample01 : 내일까지</h1>
+  <h2 id="sample01"></h2>
+  <br/>
+  <h1>Sample02 : 2024년 4월 1일까지
+  </h1>
+    <h2 id="sample02"></h2>
+    <h2 id="sample03"></h2>
+  <br/>
+  <h1>Sample03 : 종료 케이스
+  </h1>
+  <h2 id="sample04"></h2> -->
+  <div ref="counttest">ddddd</div>
 </div>
 </template>
 <script>
@@ -18,8 +28,14 @@ export default {
     },
     created() {
         //this.countDownTimer();
-
+        this.countTimer();
+        //console.log('hi')
+        //console.log('refs', this.$refs.counttest.focus() );
     },
+    mounted(){
+         this.countDownTimer('04/01/2024 00:00 AM', this.$refs.counttest.innerText);
+    },
+
 
     computed: {
       ...mapGetters(['menus']),
@@ -39,42 +55,37 @@ export default {
             }
         },
 
-        countTimer () {
-          const countDownTimer = function (id, date) {
-            var _vDate = new Date(date); // 전달 받은 일자
-            var _second = 1000;
-            var _minute = _second * 60;
-            var _hour = _minute * 60;
-            var _day = _hour * 24;
-            var timer;
-            function showRemaining() {
-              var now = new Date();
-              var distDt = _vDate - now;
-              if (distDt < 0) {
-                 clearInterval(timer); document.getElementById(id).textContent = '해당 이벤트가 종료 되었습니다!';
-                 return;
-                }
-              var days = Math.floor(distDt / _day);
-              var hours = Math.floor((distDt % _day) / _hour);
-              var minutes = Math.floor((distDt % _hour) / _minute);
-              var seconds = Math.floor((distDt % _minute) / _second); //document.getElementById(id).textContent = date.toLocaleString() + "까지 : ";
 
-              document.getElementById(id).textContent = days + '일 ';
-              document.getElementById(id).textContent += hours + '시간 ';
-              document.getElementById(id).textContent += minutes + '분 ';
-              document.getElementById(id).textContent += seconds + '초'; }
+         // this.$refs.counttest.innerText = "hi"
 
-              timer = setInterval(showRemaining, 1000);
+          countDownTimer (date) {
+                let _vDate = new Date(date, id); // 전달 받은 일자
+                let _second = 1000;
+                let _minute = _second * 60;
+                let _hour = _minute * 60;
+                let _day = _hour * 24;
+                let timer;
+                function showRemaining() {
+                    let now = new Date();
+                    let distDt = _vDate - now;
+                    if (distDt < 0) {
+                      clearInterval(timer);
+                      this.$refs.countTest.innerText = '해당 이벤트가 종료 되었습니다!';
+                      return;
+                      }
+                    let days = Math.floor(distDt / _day);
+                    let hours = Math.floor((distDt % _day) / _hour);
+                    let minutes = Math.floor((distDt % _hour) / _minute);
+                    let seconds = Math.floor((distDt % _minute) / _second); //document.getElementById(id).textContent = date.toLocaleString() + "까지 : ";
 
-              }
-              var dateObj = new Date(); dateObj.setDate(dateObj.getDate() + 1);
-              countDownTimer('sample01', dateObj); // 내일까지
-              countDownTimer('sample02', '04/01/2024 00:00 AM'); // 2024년 4월 1일까지, 시간을 표시하려면 01:00 AM과 같은 형식을 사용한다.
-              countDownTimer('sample03', '04/01/2024'); // 2024년 4월 1일까지
-              countDownTimer('sample04', '04/01/2019'); // 2024년 4월 1일까지
+                    id = days + '일 ';
+                    //this.$refs.counttest.innerText += hours + '시간 ';
+                    //this.$refs.counttest.innerText += minutes + '분 ';
+                    //this.$refs.counttest.innerText += seconds + '초';
+                  }
+                timer = setInterval(showRemaining, 1000);
 
-
-        }
+            }
     },
 
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <!-- <CountDown/> -->
+     <CountDown/>
   </div>
 </template>
 
